1e58df35ee0d6020dc3bcea5ab1ba7d28abd8417
Sashini Shihara <sashinisr98@gmail.com>
2021-07-10 14:21:56 +0530

    admin account

--- cantata/src/Screens/AdminPage/Components/Dashboard/Components/CountBox/index.js
+++ cantata/src/Screens/AdminPage/Components/Dashboard/Components/CountBox/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./index.css";
 
 function CountBox() {
+    const bx={
+        backgroundColor:"rgb(204, 197, 197)",
+             height: "90%",
+        width: "80%"
+    }
 
     return (
         <div>
             <nav class="level mt-6">
-                <div className="level-item has-text-centered">
+                <div className="level-item has-text-centered" >
                     <div>
                         <div className="box mx-6">
                             <div class="content">
@@ -20,7 +25,7 @@ function CountBox() {
                 </div>
                 <div class="level-item has-text-centered">
                     <div>
-                    <div class="box mx-6">
+                    <div class="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
                                     <h1>Users</h1>
@@ -32,7 +37,7 @@ function CountBox() {
                 </div>
                 <div class="level-item has-text-centered">
                     <div>
-                    <div class="box mx-6">
+                    <div class="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
                                     <h1>Posts</h1>
--- cantata/src/Screens/AdminPage/Components/ReportedUsers1/Components/CountBoxUser/index.js
+++ cantata/src/Screens/AdminPage/Components/ReportedUsers1/Components/CountBoxUser/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function CountBoxUser(){
+    const bx={
+        backgroundColor:"rgb(204, 197, 197)",
+             height: "90%",
+        width: "80%"
+    }
     return(
         <div>
             <nav class="level mt-6">
                 <div className="level-item has-text-centered">
                     <div>
-                        <div className="box mx-6">
+                        <div className="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
                                     <h1>9864</h1>
@@ -18,7 +23,7 @@ function CountBoxUser(){
                 </div>
                 <div class="level-item has-text-centered">
                     <div>
-                    <div class="box mx-6">
+                    <div class="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
                                     <h1>4125</h1>
@@ -30,7 +35,7 @@ function CountBoxUser(){
                 </div>
                 <div class="level-item has-text-centered">
                     <div>
-                    <div class="box mx-6">
+                    <div class="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
                                     <h1>5245</h1>
