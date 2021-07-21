4b73af0500b7ada6987a7b878e5f9abb0e8d306f
Sashini Shihara <sashinisr98@gmail.com>
2021-07-21 18:17:23 +0530

    test

--- src/Screens/AdminPage/Components/UserManagement/Components/Navtab/Components/ActiveUser/index.js
+++ src/Screens/AdminPage/Components/UserManagement/Components/Navtab/Components/ActiveUser/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from "axios";
+import { render } from "@testing-library/react";
 
-function ActiveUser() {
+function ActiveUser(props){
     const tbl = {
         width: "100%"
-    }
+    };
 
-    
+    const [search,setSearch] =useState('');
+    const [record,setRecord] = useState([]);
+
+    const searchRecords = () =>
+    {
+        console.log("workin");
+        axios.get(`http://localhost:5000/api/admin/1`)
+        .then(response => {
+            console.log(response.data)
+          setRecord(response.data);
+        });
+         
+    }
+ 
     return (
         <div>
+              <select id="sel" onChange={searchRecords}>
+                            <option value="all">All Users</option>
+                            <option value="specify">Selected Users</option>
+                        </select>
             <table class="table" style={tbl}>
                 <thead>
                     <tr>
