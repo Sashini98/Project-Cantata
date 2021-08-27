ea4b0e4e6eadbdb201166e7bd25d7d6c501e61a8
Sashini Shihara <sashinisr98@gmail.com>
2021-08-27 12:12:41 +0530

    Info Boxes Final

--- src/Screens/AdminPage/Components/ReporedPosts1/Components/CountBox/index.js
+++ src/Screens/AdminPage/Components/ReporedPosts1/Components/CountBox/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import "./index.css";
 
 
@@ -8,17 +9,32 @@ function CountBox() {
              height: "90%",
         width: "80%"
     }
+    const [record, setRecord] = useState([]);
 
+    const loadCounts = async () => {
+        var response = fetch('http://localhost:5000/api/v1/admin/getcovercounts')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                setRecord(myJson);
+            });
+    }
+    useEffect(() => {
+        loadCounts();
+    }, []);
 
     return (
         <div>
+               <table>
+                {record.map((counts) =>
             <nav class="level mt-6">
                 <div className="level-item has-text-centered">
                     <div>
                         <div className="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
-                                    <h1>9864</h1>
+                                <h1> {counts.Tot}</h1>
                                     Reports
                                 </p>
                             </div>
@@ -30,7 +46,7 @@ function CountBox() {
                     <div class="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
-                                    <h1>4125</h1>
+                                <h1> {counts.Deactive}</h1>
                                     Resolved
                                 </p>
                             </div>
@@ -42,7 +58,7 @@ function CountBox() {
                     <div class="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
-                                    <h1>5245</h1>
+                                <h1> {counts.Active}</h1>
                                     Unread
                                 </p>
                             </div>
@@ -52,6 +68,8 @@ function CountBox() {
 
 
             </nav>
+            )}
+            </table>
         </div>
 
 
--- src/Screens/AdminPage/Components/ReportedUsers1/Components/CountBoxUser/index.js
+++ src/Screens/AdminPage/Components/ReportedUsers1/Components/CountBoxUser/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 
 function CountBoxUser(){
     const bx={
@@ -6,15 +7,32 @@ function CountBoxUser(){
              height: "90%",
         width: "80%"
     }
+
+    const [record, setRecord] = useState([]);
+
+    const loadCounts = async () => {
+        var response = fetch('http://localhost:5000/api/v1/admin/getusercounts')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                setRecord(myJson);
+            });
+    }
+    useEffect(() => {
+        loadCounts();
+    }, []);
     return(
         <div>
+             <table>
+                {record.map((counts) =>
             <nav class="level mt-6">
                 <div className="level-item has-text-centered">
                     <div>
                         <div className="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
-                                    <h1>9864</h1>
+                                <h1> {counts.Tot}</h1>
                                     Reports
                                 </p>
                             </div>
@@ -26,7 +44,7 @@ function CountBoxUser(){
                     <div class="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
-                                    <h1>4125</h1>
+                                <h1> {counts.Deactive}</h1>
                                     Resolved
                                 </p>
                             </div>
@@ -38,7 +56,7 @@ function CountBoxUser(){
                     <div class="box mx-6"style={bx}>
                             <div class="content">
                                 <p>
-                                    <h1>5245</h1>
+                                <h1> {counts.Active}</h1>
                                     Unread
                                 </p>
                             </div>
@@ -48,6 +66,8 @@ function CountBoxUser(){
 
 
             </nav>
+            )}
+            </table>
         </div>
     )
 }
