7308a4e89519a0c0dcb5c861d12a526eea9b9436
Sashini Shihara <sashinisr98@gmail.com>
2021-09-08 18:30:05 +0530

    changes

--- src/Screens/AdminPage/Components/Dashboard/Components/TrafficBar/index.js
+++ src/Screens/AdminPage/Components/Dashboard/Components/TrafficBar/index.js
@@ -13,7 +13,6 @@ function TrafficBar() {
 
   
 useEffect(() => {
-  // window.location.reload(false);
   loaduserCounts();
   loadcoverCounts();
   loadlyricCounts();
@@ -57,19 +56,19 @@ useEffect(() => {
 
 
 const data = [
-  { name: 'January', Covers: recordcover[0].covercnt, Users: recorduser[0].usercnt, Lyrics: recordlyric[0].lyriccnt },
-  { name: 'February', Covers: recordcover[1].covercnt, Users: recorduser[1].usercnt, Lyrics: recordlyric[1].lyriccnt },
-  { name: 'March', Covers: recordcover[2].covercnt, Users: recorduser[2].usercnt, Lyrics: recordlyric[2].lyriccnt },
-  { name: 'April', Covers: recordcover[3].covercnt, Users: recorduser[3].usercnt, Lyrics: recordlyric[3].lyriccnt },
-  { name: 'May', Covers: recordcover[4].covercnt, Users: recorduser[4].usercnt, Lyrics: recordlyric[4].lyriccnt },
-  { name: 'June', Covers: recordcover[5].covercnt, Users: recorduser[5].usercnt, Lyrics: recordlyric[5].lyriccnt },
-  { name: 'July', Covers: recordcover[6].covercnt, Users: recorduser[6].usercnt, Lyrics: recordlyric[6].lyriccnt },
-  { name: 'August', Covers: recordcover[7].covercnt, Users: recorduser[7].usercnt, Lyrics: recordlyric[7].lyriccnt },
-  { name: 'September', Covers: recordcover[8].covercnt, Users: recorduser[8].usercnt, Lyrics: recordlyric[8].lyriccnt },
-  { name: 'October', Covers: recordcover[9].covercnt, Users: recorduser[9].usercnt, Lyrics: recordlyric[9].lyriccnt },
-  { name: 'November', Covers: recordcover[10].covercnt, Users: recorduser[10].usercnt, Lyrics: recordlyric[10].lyriccnt },
-  { name: 'December', Covers: recordcover[11].covercnt, Users: recorduser[11].usercnt, Lyrics: recordlyric[11].lyriccnt },
-
+  { name: 'January', Covers: recordcover || recordcover[0].covercnt, Users: recorduser || recorduser[0].usercnt, Lyrics: recordlyric || recordlyric[0].lyriccnt },
+  { name: 'February', Covers: recordcover || recordcover[1].covercnt, Users: recorduser || recorduser[1].usercnt, Lyrics: recordlyric || recordlyric[1].lyriccnt },
+  { name: 'March', Covers: recordcover || recordcover[2].covercnt, Users: recorduser || recorduser[2].usercnt, Lyrics: recordlyric || recordlyric[2].lyriccnt },
+  { name: 'April', Covers: recordcover || recordcover[3].covercnt, Users: recorduser || recorduser[3].usercnt, Lyrics: recordlyric || recordlyric[3].lyriccnt },
+  { name: 'May', Covers: recordcover || recordcover[4].covercnt, Users: recorduser || recorduser[4].usercnt, Lyrics: recordlyric || recordlyric[4].lyriccnt },
+  { name: 'June', Covers: recordcover || recordcover[5].covercnt, Users: recorduser || recorduser[5].usercnt, Lyrics: recordlyric || recordlyric[5].lyriccnt },
+  { name: 'July', Covers: recordcover || recordcover[6].covercnt, Users: recorduser || recorduser[6].usercnt, Lyrics: recordlyric || recordlyric[6].lyriccnt },
+  { name: 'August', Covers: recordcover || recordcover[7].covercnt, Users: recorduser || recorduser[7].usercnt, Lyrics: recordlyric || recordlyric[7].lyriccnt },
+  { name: 'September', Covers: recordcover || recordcover[8].covercnt, Users: recorduser || recorduser[8].usercnt, Lyrics: recordlyric || recordlyric[8].lyriccnt },
+  { name: 'October', Covers: recordcover || recordcover[9].covercnt, Users: recorduser || recorduser[9].usercnt, Lyrics: recordlyric || recordlyric[9].lyriccnt },
+  { name: 'November', Covers: recordcover || recordcover[10].covercnt, Users: recorduser || recorduser[10].usercnt, Lyrics: recordlyric || recordlyric[10].lyriccnt },
+  { name: 'December', Covers: recordcover || recordcover[11].covercnt, Users: recorduser || recorduser[11].usercnt, Lyrics: recordlyric || recordlyric[11].lyriccnt }
+  
 ];
 
 
@@ -88,6 +87,7 @@ return (
         <Line type="monotone" dataKey="Lyrics" stroke="#8456d8" />
 
       </LineChart>
+
     </div>
 
   </div>
