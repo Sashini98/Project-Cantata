07d8a712fe3ede9ef96edf8e813e4752f9944eba
hiradev <73779491+devindi-177@users.noreply.github.com>
2021-09-20 02:55:51 +0530

    Update index.js

--- src/Screens/UserProfile/Components/SideNav/index.js
+++ src/Screens/UserProfile/Components/SideNav/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import logo from "../../../../Assets/Admin/logo2.png";
+import icon from "../../../../Assets/Admin/Icon.png";
 import user from "../../../../Assets/Admin/repuser.png";
 import usermgt from "../../../../Assets/Admin/usermgt.png";
 import notify from "../../../../Assets/Admin/notify.png";
 import lgout from "../../../../Assets/Admin/logout.png";
 
 import './index.css';
+import { Icon } from "@material-ui/core";
 
 const imgstyle = {
     paddingRight: "3vh"
+}
+
+const sideNavLetters = {
+    color: "#ffdd57",
+    "&:hover": { color: "#0000" }
 }
 
 function SideNav() {
@@ -21,17 +27,19 @@ function SideNav() {
 
         <aside className="menu drawer-menu">
 
-            <div className="navtop is-centered">
-                <img src={logo} ></img>
+            <div className="nav-top is-centered">
+                <img src={icon} ></img>
 
 
             </div>
 
-            <ul className="menu-list">
-                <li><a className={subpath === "./index.js" ? "is-active" : ""}><Link to="UserProfile"><img src={user} style={imgstyle} />User Profile</Link></a></li>
-                <li><a className={subpath === "../EditProfile/index.js" ? "is-active" : ""}><Link to="editprofile"><img src={usermgt} style={imgstyle} />Edit Profile</Link></a></li>
-                <li><a className={subpath === "../Notification/index.js" ? "is-active" : ""}><Link to="notification"><img src={notify} style={imgstyle} />Notification</Link></a></li>
-                <li><a><Link to="/"><img src={lgout} style={imgstyle} />Logout</Link></a></li>
+
+            <ul className="menu-list side">
+                <li><a className={subpath === "./index.js" ? "is-active" : ""}><Link to="UserProfile"><img src={user} style={imgstyle} /><span className="side-letters" style={sideNavLetters}>User Profile</span></Link></a></li>
+                <li><a className={subpath === "../EditProfile/index.js" ? "is-active" : ""}><Link to="editprofile"><img src={usermgt} style={imgstyle} /><span className="side-letters" style={sideNavLetters}>Edit Profile</span></Link></a></li>
+                <li><a className={subpath === "../Notification/index.js" ? "is-active" : ""}><Link to="notification"><img src={notify} style={imgstyle} /><span className="side-letters" style={sideNavLetters}>Notification</span></Link></a></li>
+                <li><a><Link to="/"><img src={lgout} style={imgstyle} /><span className="side-letters" style={sideNavLetters}>Logout</span></Link></a></li>
+
             </ul>
 
         </aside>
@@ -39,5 +47,4 @@ function SideNav() {
 }
 
 export default SideNav;
-
 
