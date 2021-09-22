5ae7187ce9bcc0ed3b4f432b42faab531fd0239d
BhagyaGunathilaka <2018cs059@stu.ucsc.cmb.ac.lk>
2021-09-22 18:00:42 +0530

    Search

--- src/Screens/Feed/Components/TopNav/index.js
+++ src/Screens/Feed/Components/TopNav/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import user from "../../../../Assets/Admin/user.png";
+import { FaSearch } from "react-icons/fa";
 
 import "./index.css";
 function TopNav() {
@@ -8,6 +9,20 @@ function TopNav() {
 	return (
 		<nav className="feed-navbar" role="navigation" aria-label="main navigation">
 			<div className="navbar-end">
+				<div className="navbar-item">
+					<div class="wrap">
+						<div class="search">
+							<input
+								type="text"
+								class="searchTerm"
+								placeholder="What are you looking for?"
+							></input>
+							<button type="submit" class="searchButton" onClick="">
+								<FaSearch />
+							</button>
+						</div>
+					</div>
+				</div>
 				<div className="navbar-item">
 					<p className="navbar-name">
 						<strong>
